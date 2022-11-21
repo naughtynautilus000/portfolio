ce15b9e3e96210f680c4fd2d2cd298766825b958
Nathan Andrews <nandrews.programming@gmail.com>
2022-11-21 15:03:55 -0500

    refactored styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			colors: {
-				'white': '#cee8f3',
-				'highlight': '#52e5b9',
-				'background': '#0a192f',
-				'blue': '#319fce',
-				'dark-blue': '#10284C'
-			}
+		colors: {
+			'white': '#cee8f3',
+			'highlight': '#52e5b9',
+			'background': '#0a192f',
+			'blue': '#319fce',
+			'dark-blue': '#10284C'
 		},
+		extend: {
+			width: {
+				'sides': '150px'
+			},
+			height: {
+				'header': '75px'
+			}
+		}
 	},
 	plugins: [],
 }
